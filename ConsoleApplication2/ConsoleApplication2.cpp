211eee0f57c217b5d7147a4f8f7d2970e6aa347a
dmitri <dmitri@WIN-BJOJVFO9MOA>
2024-06-05 13:01:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void drawTriangle(int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < n - i; ++j) {
+            cout << ' ';
+        }
+        for (int k = 0; k < 2 * i - 1; ++k) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int height;
+    cin >> height;
+    drawTriangle(height);
+    return 0;
+}
+
+
